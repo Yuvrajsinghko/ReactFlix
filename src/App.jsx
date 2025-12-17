9196a44267f7883e582f96d2c02f3ccd924faa38
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-12-17 22:09:05 +0530

    completed  header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import Search from "./components/Search";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [searchTerm, setsearchTerm] = useState("");
 
-export default App
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <img src="./hero.png" alt="Hero Banner" />
+        <header>
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without Hassel
+          </h1>
+        </header>
+        <Search searchTerm={searchTerm} setsearchTerm={setsearchTerm}/>
+        
+      </div>
+    </main>
+  );
+};
+export default App;
